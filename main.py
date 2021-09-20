8a114ce3d5cdd669ab0ceff5760b438bd30acf7c
Ilirys <lilianmarie45@gmail.com>
2021-09-20 14:56:31 +0200

    Ajout logo + commentaire

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ GREEN = (0,255,0)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 pygame.display.set_caption("Age of Cheap Empire") #Nom du jeu affiché sur la fenetre
+icon = pygame.image.load('Age_of_Empires_franchise_logo.png')
+pygame.display.set_icon(icon) #Logo de la fenêtre
 
 def drawGrid():
     blocksize = 20 #La taille d'un carré 
     for x in range(0,WIDTH,blocksize):
         for y in range(0,HEIGHT,blocksize):
-            rect = pygame.Rect(x, y, blocksize, blocksize) # rect est un objet qui store des coordonnées rectangulaire. (A utiliser pour placer les batiments, tout déplacement..?)
+            rect = pygame.Rect(x, y, blocksize, blocksize) # rect est un objet qui store des coordonnées rectangulaire. 
+                                                           #(A utiliser pour placer les batiments, tout déplacement..?)
             pygame.draw.rect(SCREEN, WHITE, rect, 1)
 
 
@@ -30,7 +33,6 @@ def main():
                 pygame.quit()
                 os.sys.exit()
         pygame.display.update()
-    pygame.quit()   #Quite proprement en cas de plantage
 
              
 
