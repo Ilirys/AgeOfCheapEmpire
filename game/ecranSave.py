c5d2f3f9cd9c1a088e539e4437090e4f8d97d344
Léonard <leonard.maurice@insa-cvl.fr>
2021-12-12 22:19:22 +0100

    update menu echap

--- game/ecranSave.py
+++ game/ecranSave.py
@@ -5,69 +5,83 @@ from .definitions import *
 from pygame.locals import *
 
 
+
 class Save:
     click = False
 
-    def __init__(self, screen, clock):
+    def __init__(self, screen, clock, game):
         self.screen = screen
         self.clock = clock
+        self.game = game
         self.width, self.height = self.screen.get_size()
 
+
+
     def ecran_options(self):  # fonction pour afficher l'écran de démarrage
-        self.playing = True
-        while self.playing:
 
-            self.screen.fill((0, 0, 0))  # arrière plan
+        click = False
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    tmp = 1
+                    while (tmp == 1):
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        if event.type == MOUSEBUTTONDOWN:
+                            if event.button == 1:
+                                click = True
 
-            image = pygame.image.load('assets/menu_options.png').convert_alpha()
-            self.screen.blit(image, (750, 390))
+                        image = pygame.image.load('assets/menu_options.png').convert_alpha()
+                        self.screen.blit(image, (750, 390))
 
-            mx, my = pygame.mouse.get_pos()  # récupération des clics de souris
+                        mx, my = pygame.mouse.get_pos()  # récupération des clics de souris
 
-            button_1 = pygame.Rect(898, 510, 176, 35)  # les boutons (ici, invisibles) -> bouton quitter
-            button_2 = pygame.Rect(898, 560, 176, 35)  # -> bouton charger
-            button_3 = pygame.Rect(898, 605, 176, 35)  # -> bouton save
-            button_4 = pygame.Rect(898, 655, 176, 35)  # -> bouton annuler
+                        button_1 = pygame.Rect(898, 510, 176, 35)  # les boutons (ici, invisibles) -> bouton quitter
+                        button_2 = pygame.Rect(898, 560, 176, 35)  # -> bouton charger
+                        button_3 = pygame.Rect(898, 605, 176, 35)  # -> bouton save
+                        button_4 = pygame.Rect(898, 655, 176, 35)  # -> bouton annuler
 
-            if button_1.collidepoint((mx, my)):  # zone de collision des boutons et action quand cliqué
-                pygame.draw.rect(self.screen, (255, 255, 255), (896, 518, 180, 40), 3)
-                if click:
-                    pygame.quit()
-                    sys.exit()
-            if button_2.collidepoint((mx, my)):
-                pygame.draw.rect(self.screen, (255, 255, 255), (896, 568, 180, 40), 3)
-                # if click:
-                # charger sauvegarde
-            if button_3.collidepoint((mx, my)):
-                pygame.draw.rect(self.screen, (255, 255, 255), (896, 613, 180, 40), 3)
-                # if click:
-                # save david
-            if button_4.collidepoint((mx, my)):
-                pygame.draw.rect(self.screen, (255, 255, 255), (896, 663, 180, 40), 3)
-                # if click:
-                # annuler
+                        if button_1.collidepoint((mx, my)):  # zone de collision des boutons et action quand cliqué
+                            pygame.draw.rect(self.screen, (255, 255, 255), (896, 518, 180, 40), 3)
+                            if click:
+                                pygame.quit()
+                                sys.exit()
+                        if button_2.collidepoint((mx, my)):
+                            pygame.draw.rect(self.screen, (255, 255, 255), (896, 568, 180, 40), 3)
+                            # if click:
+                            # charger sauvegarde
+                        if button_3.collidepoint((mx, my)):
+                            pygame.draw.rect(self.screen, (255, 255, 255), (896, 613, 180, 40), 3)
+                            if click:
+                                self.game.resource_manager.save()
+                                self.game.save()
+                                self.game.world.save()
 
 
-            # dessin des boutons pour s'assurer de leur bonne position
-            # pygame.draw.rect(self.screen, (255, 215, 50), button_1)
-            # pygame.draw.rect(self.screen, (255, 215, 50), button_2)
-            # pygame.draw.rect(self.screen, (255, 215, 50), button_3)
-            # pygame.draw.rect(self.screen, (255, 215, 50), button_4)
+                        if button_4.collidepoint((mx, my)):
+                            pygame.draw.rect(self.screen, (255, 255, 255), (896, 663, 180, 40), 3)
+                            if click:
+                                tmp = 0
 
-            click = False
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
+                        # dessin des boutons pour s'assurer de leur bonne position
+                        pygame.draw.rect(self.screen, (255, 215, 50), button_1)
+                        pygame.draw.rect(self.screen, (255, 215, 50), button_2)
+                        pygame.draw.rect(self.screen, (255, 215, 50), button_3)
+                        pygame.draw.rect(self.screen, (255, 215, 50), button_4)
 
-            pygame.display.update()
-            self.clock.tick(60)
+                        click = False
+                        for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                pygame.quit()
+                                sys.exit()
+                            if event.type == KEYDOWN:
+                                if event.key == K_ESCAPE:
+                                    pygame.quit()
+                                    sys.exit()
+                            if event.type == MOUSEBUTTONDOWN:
+                                if event.button == 1:
+                                    click = True
 
-    # ecran_demarrage()
+                        pygame.display.update()
+                        self.clock.tick(60)
--- game/game.py
+++ game/game.py
@@ -21,6 +21,7 @@ from .chat import Chat
 from .Ressource import Ressource
 from .soldier import Soldier
 from .horseman import Horseman
+from .ecranSave import Save
 
 class Game:
 
@@ -53,6 +54,9 @@ class Game:
         #Chat
         self.chat = Chat(self.resource_manager, 15, 100, 200, 30)
 
+        #Menu
+        self.ecran_options = Save(self.screen, self.clock, self)
+
         #Unité
         # Worker(self.world.world[0][0], self.world,self.camera)
         # Horseman(self.world.world[0][1], self.world,self.camera)
@@ -68,17 +72,12 @@ class Game:
             self.events()
             self.update()
             self.draw()
+            self.ecran_options.ecran_options()
 
 
 
     def events(self):
         for event in pygame.event.get(): # Si on clique sur la croix pour quitter, on arrete le jeu
-            if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
-                self.resource_manager.save()
-                self.save()
-                self.world.save()
-                pygame.quit()
-                sys.exit()
             self.chat.handle_event(event)
 
 
