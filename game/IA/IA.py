eb3a061c774769966d9278a2f5fde4d2528a40f0
David <david.legrand@insa-cvl.fr>
2022-01-04 19:31:19 +0100

    fix out of bound error

--- game/IA/IA.py
+++ game/IA/IA.py
@@ -80,7 +80,7 @@ class IA:
             self.build(name_of_building)
             self.build_position_x, self.build_position_y = build_save[0], build_save[1]
 
-        print(self.count_x, self.count_y, self.next_val)
+        # print(self.count_x, self.count_y, self.next_val)
         self.count_x, self.count_y = self.count_y, self.count_x  #Switch
         self.next_val = next(self.switch_iterator)
         self.next_val_2 = next(self.switch_iterator_2)
@@ -89,34 +89,35 @@ class IA:
 
 
     def build(self, name_of_building):  #Placer le batiment
-        collision2 = self.world.world[self.build_position_x + 1][self.build_position_y]["collision"]
-        collision3 = self.world.world[self.build_position_x + 1][self.build_position_y + 1]["collision"]
-        collision4 = self.world.world[self.build_position_x ][self.build_position_y - 1]["collision"]
-
-        if dicoBatiment[name_of_building][1] == 1: #Pour taille de batiment 1x1
-            ent = Batiment(self.world.world[self.build_position_x][self.build_position_y]["render_pos"], name_of_building, self.ressource_manager)
-            self.world.entities.append(ent)
-            ent.current_image = 1   #Petite image ruine pour construction
-            self.world.batiment[self.build_position_x][self.build_position_y] = ent
-            self.world.world[self.build_position_x][self.build_position_y]["collision"] = True
-            self.world.collision_matrix[self.build_position_y][self.build_position_x] = 0
-            self.world.world[self.build_position_x][self.build_position_y]["tile"].tile_batiment = self.world.world[self.build_position_x][self.build_position_y]
-            self.ordre_de_construction_villageois(self.build_position_x, self.build_position_y)
-            print(name_of_building, " has been built successfuly")
-
-        elif ((not collision2) and (not collision3) and (not collision4)):  # les 3 autres cases sont dispos
-            ent = Batiment(self.world.world[self.build_position_x][self.build_position_y]["render_pos"], name_of_building, self.ressource_manager)
-            self.world.entities.append(ent)
-            self.world.batiment[self.build_position_x][self.build_position_y] = ent
-            for i in range (dicoBatiment[name_of_building][1]):
-                for j in range (dicoBatiment[name_of_building][1]):
-                    self.world.world[self.build_position_x+i][self.build_position_y+j]["collision"] = True
-                    self.world.collision_matrix[self.build_position_y+j][self.build_position_x+i] = 0
-                    self.world.world[self.build_position_x + i][self.build_position_y + j]["tile"].tile_batiment = self.world.world[self.build_position_x][self.build_position_y]
-
-            self.ordre_de_construction_villageois(self.build_position_x, self.build_position_y)
-            print(name_of_building, " has been built successfuly")
+        if self.build_position_x != self.towncenter["grid"][0] and self.build_position_y != self.towncenter["grid"][1]:    
+            if self.build_position_x + 1 in range(self.world.grid_length_x) and self.build_position_y + 1 in range(self.world.grid_length_y):
+                collision2 = self.world.world[self.build_position_x + 1][self.build_position_y]["collision"]
+                collision3 = self.world.world[self.build_position_x + 1][self.build_position_y + 1]["collision"]
+                collision4 = self.world.world[self.build_position_x ][self.build_position_y - 1]["collision"]
             
+            if dicoBatiment[name_of_building][1] == 1: #Pour taille de batiment 1x1
+                ent = Batiment(self.world.world[self.build_position_x][self.build_position_y]["render_pos"], name_of_building, self.ressource_manager)
+                self.world.entities.append(ent)
+                ent.current_image = 1   #Petite image ruine pour construction
+                self.world.batiment[self.build_position_x][self.build_position_y] = ent
+                self.world.world[self.build_position_x][self.build_position_y]["collision"] = True
+                self.world.collision_matrix[self.build_position_y][self.build_position_x] = 0
+                self.world.world[self.build_position_x][self.build_position_y]["tile"].tile_batiment = self.world.world[self.build_position_x][self.build_position_y]
+                self.ordre_de_construction_villageois(self.build_position_x, self.build_position_y)
+                print(name_of_building, " has been built successfuly")
+
+            elif (self.build_position_x + 1 in range(self.world.grid_length_x) and self.build_position_y + 1 in range(self.world.grid_length_y) and (not collision2) and (not collision3) and (not collision4) ):  # les 3 autres cases sont dispos
+                ent = Batiment(self.world.world[self.build_position_x][self.build_position_y]["render_pos"], name_of_building, self.ressource_manager)
+                self.world.entities.append(ent)
+                self.world.batiment[self.build_position_x][self.build_position_y] = ent
+                for i in range (dicoBatiment[name_of_building][1]):
+                    for j in range (dicoBatiment[name_of_building][1]):
+                        self.world.world[self.build_position_x+i][self.build_position_y+j]["collision"] = True
+                        self.world.collision_matrix[self.build_position_y+j][self.build_position_x+i] = 0
+                        self.world.world[self.build_position_x + i][self.build_position_y + j]["tile"].tile_batiment = self.world.world[self.build_position_x][self.build_position_y]
+
+                self.ordre_de_construction_villageois(self.build_position_x, self.build_position_y)
+                print(name_of_building, " has been built successfuly")
     def ordre_de_construction_villageois(self, grid_pos_x, grid_pos_y): #Ordonner a un villageois de construire
        for villager_x in self.villagers:  # Pour que le villageois construise un batiment, on trouve le villageois selectionné
            for villager in villager_x:
